1f416e50bb684f0ed681c695a81c8fffea21dba1
Aaron Askew <aaronaskew@gmail.com>
2024-05-29 14:47:32 -0500

    day 10

--- day-10/src/bin/part2.rs
+++ day-10/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = include_str!("input1.txt");
@@ -15,6 +15,7 @@ fn main() {
 // . is ground; there is no pipe in this tile.
 // S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
 
+#[derive(PartialEq)]
 enum Direction {
     North,
     South,
@@ -59,7 +60,8 @@ impl Pipe {
 
 fn process(input: &str) -> String {
     let mut grid = HashMap::<Position, Pipe>::new();
-
+    let size = (input.lines().next().unwrap().len(), input.lines().count());
+    println!("size: {size:?}");
     let mut start_pos: Position = (0, 0);
 
     for (row, line) in input.lines().enumerate() {
@@ -134,7 +136,11 @@ fn process(input: &str) -> String {
     let mut curr_pos = start_pos;
     let mut last_pos = start_pos;
 
+    let mut loop_positions = HashSet::<Position>::new();
+
     loop {
+        loop_positions.insert(curr_pos);
+
         let curr_pipe = grid.get(&curr_pos).unwrap();
 
         let next_pos = if curr_pipe.can_go(Direction::North)
@@ -162,7 +168,90 @@ fn process(input: &str) -> String {
 
     println!("steps: {steps} half: {}", steps / 2);
 
-    format!("{}", steps / 2)
+    for row in 0..size.1 {
+        for col in 0..size.0 {
+            if loop_positions.contains(&(col, row)) {
+                print!(
+                    "{}",
+                    match grid.get(&(col, row)).unwrap() {
+                        Pipe::NorthSouth => "|",
+                        Pipe::EastWest => "-",
+                        Pipe::NorthEast => "L",
+                        Pipe::NorthWest => "J",
+                        Pipe::SouthWest => "7",
+                        Pipe::SouthEast => "F",
+                        Pipe::Nothing => "#",
+                    }
+                );
+            } else {
+                print!(".")
+            }
+        }
+        println!();
+    }
+
+    let mut contained_area = 0;
+    let mut is_inside_loop;
+
+    for row in 0..size.1 {
+        for col in 0..size.0 {
+            if loop_positions.contains(&(col, row)) {
+                continue;
+            } else {
+                is_inside_loop = false;
+                let mut bends: Vec<Direction> = Vec::new();
+
+                // count the boundary crossings to the left
+                if col == 0 {
+                    continue;
+                } else {
+                    let i = col - 1;
+                    for x in (0..=i).rev() {
+                        // println!("pos: {:?}", (x, row));
+                        let pipe = grid.get(&(x, row)).unwrap();
+                        if loop_positions.contains(&(x, row)) {
+                            match *pipe {
+                                Pipe::NorthSouth => {
+                                    is_inside_loop = !is_inside_loop;
+                                }
+                                Pipe::NorthEast | Pipe::NorthWest => {
+                                    if bends.is_empty() {
+                                        bends.push(Direction::North);
+                                    } else if *bends.last().unwrap() == Direction::North {
+                                        bends.pop();
+                                    } else if *bends.last().unwrap() == Direction::South {
+                                        bends.pop();
+                                        is_inside_loop = !is_inside_loop;
+                                    }
+                                }
+                                Pipe::SouthEast | Pipe::SouthWest => {
+                                    if bends.is_empty() {
+                                        bends.push(Direction::South);
+                                    } else if *bends.last().unwrap() == Direction::South {
+                                        bends.pop();
+                                    } else if *bends.last().unwrap() == Direction::North {
+                                        bends.pop();
+                                        is_inside_loop = !is_inside_loop;
+                                    }
+                                }
+                                Pipe::EastWest => {}
+                                _ => panic!("invalid pipe found"),
+                            }
+                        }
+                    }
+                    if is_inside_loop {
+                        contained_area += 1;
+                        println!("+1 (col, row) {:?}", (col, row));
+                    }
+                }
+            }
+        }
+    }
+
+    println!("contained_area: {contained_area}");
+
+    format!("{}", contained_area)
+    // todo!()
 }
 
 #[cfg(test)]
@@ -173,23 +262,49 @@ mod tests {
     #[test]
     fn example() {
         let result = process(
-            ".....
-.S-7.
-.|.|.
-.L-J.
-.....",
+            "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........",
         );
 
         assert_eq!(result, "4");
+    }
 
-        let result = process(
-            "..F7.
-.FJ|.
-SJ.L7
-|F--J
-LJ...",
-        );
+    #[test]
+    fn example2() {
+        let result = process(".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...");
 
         assert_eq!(result, "8");
+    }
+
+    #[test]
+    fn example3() {
+        let result = process("FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L");
+
+        assert_eq!(result, "10");
     }
 }
